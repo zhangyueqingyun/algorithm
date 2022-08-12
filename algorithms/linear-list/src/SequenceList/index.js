9c72356be6d2fce989a7d7305e0d08a12e09dfd4
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-12 16:26:48 +0800

    feat(linear-list): insert & delete & update

--- algorithms/linear-list/src/SequenceList/index.js
+++ algorithms/linear-list/src/SequenceList/index.js
@@ -1,5 +1,5 @@
 class SequenceList {
-    constructor(values){
+    constructor(values = []){
         this.values = values;
     }
     
@@ -8,13 +8,22 @@ class SequenceList {
      * @param {val: number} 待插入的值
      * @param {ind: number} 插入的下标
      */
-    insert(val, ind) {}
+    insert(val, ind) {
+        for(let i = this.values.length; i > ind; i--){
+            this.values[i] = this.values[i - 1];
+        }
+        this.values[i] = val;
+    }
 
     /**
      * 删除
      * @param {ind: number} 待删除的下标
      */
-    delete() {}
+    delete() {
+        for(let i = ind; i < this.values.length; i++){
+            this.values[i] = this.values[i + 1];
+        }
+    }
 
     /**
      * 更新
@@ -22,17 +31,24 @@ class SequenceList {
      * @param {ind: number} 待更新的下标
      * @return {number} 旧值
      */
-    update() {}
+    update(val, ind) {
+        const oldVal = this.values[ind]
+        this.values[ind] = val 
+        return oldVal
+    }
 
-    /**
-     * 查找
-     * @param {val: number}
-     * @return {number} 旧的下标，未找到返回 -1
-     */
-    find() {}
 
     /**
      * 排序
+     * @param {type: string} 排序算法的类型
      */
-    sort() {}
+     sort() {}
+
+    /**
+     * 查找
+     * @param {val: number} 查找的目标值
+     * @param {type: string} 查找算法的类型
+     * @return {number} 旧的下标，未找到返回 -1
+     */
+    find(val, type) {}
 }
