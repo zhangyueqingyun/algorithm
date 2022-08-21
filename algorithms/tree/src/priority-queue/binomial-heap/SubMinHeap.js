d9a3c93008aed3dae9c32f8c2bc62e57e9ef9c98
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-21 20:36:01 +0800

    feat(priority-queue): binomial heap dequeue

--- algorithms/tree/src/priority-queue/binomial-heap/SubMinHeap.js
+++ algorithms/tree/src/priority-queue/binomial-heap/SubMinHeap.js
@@ -5,13 +5,6 @@ class SubMinHeap {
         this.degree = degree;
         this.next = next;
     }
-
-    delete() {
-        const subMinHeapA = new SubMinHeap();
-        const subMinHeapB = new SubMinHeap();
-        
-        return [subMinHeapA, subMinHeapB,]
-    }
 }
 
 module.exports = SubMinHeap;
