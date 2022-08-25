f978cd9355fabdd48451a3fd896b94005ebe6dc8
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-25 10:09:47 +0800

    feat(performance): memory prode

---
+++ libs/performance/prode/memory/MemoryProde.js
@@ -0,0 +1,35 @@
+const { totalmem, freemem } = require('os');
+const { v4: uuidv4 } = require('uuid');
+const { setInterval } = require('timers/promises');
+
+class MemoryProde {
+    constructor(id) {
+        this.id = id;
+        this.#uuid = uuidv4();
+        this.#measures = [];
+        this.#finished = false;
+    }
+
+    get value() {
+        return Math.max(...this.#measures.map(measure => (measure.totalmem - measure.freemem)));
+    }
+
+    run() {
+        for await(let memory of setInterval(1, {
+            startTime: performance.now(),
+            totalmem: totalmem(),
+            freemem: freemem()
+        })) {    
+            if(this.#finished) {
+                break;
+            }
+            this.#measures.push(memory);
+        }
+    }
+
+    stop() {
+        this.#finished = true;
+    }
+}
+
+module.exports = TimeProde;
