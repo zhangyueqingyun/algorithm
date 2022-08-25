28d99e2fbda03439238d20fb474dcfcae297f289
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-25 10:23:24 +0800

    feat(performance): time prode

--- libs/performance/prode/time/TimeProde.js
+++ libs/performance/prode/time/TimeProde.js
@@ -1,9 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
 
+const RUNNING = Symbol('running');
+const WAITING = Symbol('waiting');
+const FINISHED = Symbol('finished');
+
 class TimeProde {
     constructor(id) {
         this.#id = id;
         this.#uuid = uuidv4();
+        this.#status = WAITING;
     }
 
     get value() {
@@ -20,12 +25,16 @@ class TimeProde {
     }
 
     run() {
+        if(this.#status === FINISHED) return;
+        this.#status = RUNNING;
         this.#start = performance.mark(this.#startName);
     }
 
     stop() {
+        if(this.#status === FINISHED) return;
         this.#end = performance.mark(this.#endName);
         this.#mersure =  performance.measure(this.#measureName, this.#startName, this.#endName);
+        this.#status = FINISHED;
     }
 
     get #startName () {
