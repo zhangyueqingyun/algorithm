6ed873a4f34e17f1450ac57eb104b5c8cbff6924
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-25 10:42:42 +0800

    feat(performance): measure function

---
+++ libs/performance/measure/measureFunction.js
@@ -0,0 +1,18 @@
+const {TimeProde, MemoryProde} = require('../prode');
+
+function measure(func, time = true, memory = true) {
+    const data = {}
+    time && (data.time = measureWithProde(func, TimeProde));
+    memory && (data.memory = measureWithProde(func, MemoryProde));
+    return data;
+}
+
+function measureWithProde(func, ProdeClass) {
+    const prode = new ProdeClass();
+    prode.run();
+    func();
+    prode.stop();
+    return prode.value;
+}
+
+module.exports = measure;
